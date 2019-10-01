66e0fb7b6693d48cc014e1fc588f36c222223693
HemmaLofers <lindahl.jonatan@gmail.com>
2019-10-01 23:47:36 +0200

    update

--- mydbSystem/tablesview/editAdvertiser.php
+++ mydbSystem/tablesview/editAdvertiser.php
@@ -1,9 +1,11 @@
 <?php
 
+$adver_id = $_GET["advertiser_id"];
+
 // populate table
 require_once('../mysqli_connect.php');
 
-$query = "SELECT advertiser_id, advertiser_name, advertiser_profile_image, advertiser_created, advertiser_updated FROM advertisers";
+$query = "SELECT advertiser_id, advertiser_name, advertiser_profile_image, advertiser_created, advertiser_updated FROM advertisers WHERE advertiser_id=$adver_id ";
 
 $response = @mysqli_query($dbc, $query);
 
@@ -17,34 +19,35 @@ if($response){
     <td align="left"><b>Advertiser Image</b></td>
     <td align="left"><b>Advertiser Created</b></td>
     <td align="left"><b>Advertiser Updated</b></td>
-    <td align="left"><b>Advertiser Campaigns</b></td>
-    <td align="left"><b>Edit Advertiser</b></td>
+    <td align="left"><b>Update Advertiser</b></td>
+    <td align="left"><b>Back</b></td>
     </tr>';
 
     while($row = mysqli_fetch_array($response)){
-        echo '<tr><td align="left">' .
-        $row['advertiser_id'] . '</td><td align="left">' .
-        $row['advertiser_name'] . '</td><td align="left">' .
+        echo '<tr><td align="left">
+        <form action="/campaigns.php" method="get">' .
+        $row['advertiser_id'] . '</td><td align="left">
+        <input type="text" name="advertiser_name" value="'.
+        $row['advertiser_name'] .'"/></td><td align="left">' .
         $row['advertiser_profile_image'] . '</td><td align="left">' .
         $row['advertiser_created'] . '</td><td align="left">' .
         $row['advertiser_updated'] . '</td>' .
         '<td align="left">
-        <form action="/campaigns.php" method="get">
         <input type="hidden" name="advertiser_name" value="'.
         $row['advertiser_name'] .'"/>
         <button name="advertiser_id"
         type="submit"
         value='. $row['advertiser_id'] . '>
-        View Campaigns
+        Update Advertiser
         </button>
         </form>
         </td>' .
         '<td align="left">
-        <form action="/editAdvertiser.php" method="get">
+        <form action="/index.php" method="get">
         <button name="advertiser_id"
         type="submit"
-        value='. $row['advertiser_id'] . '>
-        Edit Advertiser
+        value=null>
+        cancel
         </button>
         </form>
         </td>';
